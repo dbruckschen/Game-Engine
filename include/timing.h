1ede7f168426bbc64f203e70be557d573a02bc43
David Bruckschen <d.bruckschen@gmail.com>
2021-06-29 14:15:10 +0200

    added timing

--- include/timing.h
+++ include/timing.h
@@ -1,15 +1,22 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <windows.h>
+#include "common.h"
 
+typedef struct
+{
+    double perf_freq;
+    double start_time;
+    double end_time;
+    double elapsed_time;
 
+} Timing;
 
-
-
-
-
-
-
+void InitTiming(Timing *t);
+void StartTiming(Timing *t);
+void EndTiming(Timing *t);
+void ElapsedTiming(Timing *t);
 
 
 #endif
--- src/timing.c
+++ src/timing.c
@@ -1,19 +1,36 @@
-#include <windows.h>
-#include "common.h"
-#include "time.h"
+#include "timing.h"
 
-f64 BeginTiming(void)
+void InitTiming(Timing *t)
+{
+    LARGE_INTEGER freq = {0};
+    QueryPerformanceFrequency(&freq);
+
+    t->perf_freq = freq.QuadPart;
+}
+
+void StartTiming(Timing *t)
+{
+    LARGE_INTEGER start_time = {0};
+    QueryPerformanceCounter(&start_time);
+
+    t->start_time = start_time.QuadPart;
+}
+
+void EndTiming(Timing *t)
+{
+    LARGE_INTEGER end_time = {0};
+    QueryPerformanceCounter(&end_time);
+
+    t->end_time = end_time.QuadPart;
+
+    t->elapsed_time = t->end_time - t->start_time;
+
+}
+
+void ElapsedTiming(Timing *t)
 {
 
-    return 0.0;
 }
-	   
-
-
-
-
-
-
 
 
 
