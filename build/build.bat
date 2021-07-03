ba31302f601265bd43d1df19e80fca2d953771eb
David Bruckschen <d.bruckschen@gmail.com>
2021-07-03 21:54:55 +0200

    sprites, sanitize=address

--- build/build.bat
+++ build/build.bat
@@ -6,7 +6,7 @@ REM /Fo -> obj file folder for tmp used files
 REM link obj files with other lib files and create rml.dll
 
 
-cl /W4 /c ..\src\*.c /I ..\include /Fo"..\obj\\"
-link /DLL /OUT:rml.dll ..\obj\*.obj user32.lib gdi32.lib Ws2_32.lib
-cl /Zi /W4 ..\src\main.c /I ..\include ..\build\rml.lib 
+cl -fsanitize=address /Zi /W4 /c ..\src\*.c /I ..\include /Fo"..\obj\\"
+link /DEBUG /DLL /OUT:rml.dll ..\obj\*.obj user32.lib gdi32.lib Ws2_32.lib
+cl -fsanitize=address /Zi /W4 ..\src\main.c /I ..\include ..\build\rml.lib 
 
--- src/main.c
+++ src/main.c
@@ -19,23 +19,30 @@ int main(int argc, char **argv)
     Timer t = {0};
 
     Bitmap coin_animation[COIN_ANIM_COUNT];
-
-    Sprite Coin = {0};
-    Coin.animation_frame_count = 6
-    Coin.frames = malloc(Coin.animation_frame_count * sizeof(Bitmap));
     
+    Sprite Coin = {0};
+    Coin.animation_frame_count = COIN_ANIM_COUNT;
+    Coin.frames = malloc(Coin.animation_frame_count * sizeof(Bitmap));
+    Coin.current_frame = 0;
+    Coin.timer_next_frame = 0.2f;
+    Coin.current_timer = 0.0f;
+
     coin_animation[0] = LoadBitmapFile("coin1.bmp");
     coin_animation[1] = LoadBitmapFile("coin2.bmp");
     coin_animation[2] = LoadBitmapFile("coin3.bmp");
     coin_animation[3] = LoadBitmapFile("coin4.bmp");
     coin_animation[4] = LoadBitmapFile("coin5.bmp");
     coin_animation[5] = LoadBitmapFile("coin6.bmp");
-  
+
+    for(int i = 0; i < COIN_ANIM_COUNT; i++)
+    {
+	*(Coin.frames+i) = coin_animation[i];
+    }
+    
     InitTimer(&t);
     double timer = 0;
     u8 green = 0;
 
-    int anim_counter = 0;
     while(1)
     {
 	StartTimer(&t);
@@ -49,8 +56,10 @@ int main(int argc, char **argv)
 	
 	if(input.keyboard[a_key].toggle)
 	{
+	    FillScreen(&fbuff, RGB(0, green, 0));
+		       
 	    if(green < 255)
-		FillScreen(&fbuff, RGB(0, green++, 0));
+		green++;
 	}
 	else
 	{
@@ -58,22 +67,24 @@ int main(int argc, char **argv)
 		FillScreen(&fbuff, RGB(0, green--,  0));
 	}
   
-	if(timer >= .2f)
+	if(Coin.current_timer >= .2f)
 	{
-	    anim_counter++;
-	    timer = 0.0f;
+	    Coin.current_frame++;
+	    Coin.current_timer = 0.0f;
 	}
 	   
-	if(anim_counter > 5)
-	    anim_counter = 0;
+	if(Coin.current_frame == Coin.animation_frame_count)
+	    Coin.current_frame = 0;
 	
-	DrawBMP24bpp(&fbuff, coin_animation[anim_counter], 100, 100, RGB_Color(255, 0, 255));
+	DrawBMP24bpp(&fbuff, Coin.frames[Coin.current_frame], 100, 100, RGB_Color(255, 0, 255));
 	
 	OutputFramebuffer(window.wnd_h, fbuff);
 
 	EndTimer(&t);
 	timer += t.elapsed_time;
-	printf("%f\n", t.elapsed_time);
+	Coin.current_timer += t.elapsed_time;
+	
+	//printf("%f ms/frame\n", t.elapsed_time);
     }
 
     return 0;
