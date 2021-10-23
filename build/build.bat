4a8c3c227ecc10a2562b6ae983bf477a22dd6f56
David Bruckschen <d.bruckschen@gmail.com>
2021-10-23 22:36:14 +0200

    fixed Draw24BMP function bug where I for some reason fiddle with the src pointer of the bitmap

--- build/build.bat
+++ build/build.bat
@@ -4,9 +4,9 @@ cls
 set dllfiles=..\src\common.c ..\src\draw.c ..\src\mathlib.c ..\src\network.c ..\src\timing.c ..\src\window.c
 
 REM compile dll 
-cl -fsanitize=address /Zi /W4 /c %dllfiles% /I ..\include /Fo"..\obj\\"
+cl /Zi /Z7 /W4 /c %dllfiles% /I ..\include /Fo"..\obj\\"
 link /DEBUG /DLL /OUT:rml.dll ..\obj\*.obj user32.lib gdi32.lib Ws2_32.lib
 
 REM compile test program  
-cl -fsanitize=address /Zi /W4 /Fe:test.exe ..\test\main.c /I ..\include ..\build\rml.lib 
+cl /Zi /Z7 /W4 /Fe:test.exe ..\test\main.c /I ..\include ..\build\rml.lib 
 
