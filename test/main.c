794ef20b069bccb76cb525a880611de878dda713
David Bruckschen <d.bruckschen@gmail.com>
2021-10-24 13:55:29 +0200

    test project: drawing a tilemap

--- test/main.c
+++ test/main.c
@@ -4,15 +4,44 @@
 #include "timing.h"
 #include "common.h"
 
-#define COIN_ANIM_COUNT 6
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 600
+#define TILE_SIZE 64
+#define NUM_TILES_X (WINDOW_WIDTH / TILE_SIZE)
+#define NUM_TILES_Y (WINDOW_HEIGHT / TILE_SIZE)
+
+#define COIN_ANIM_COUNT 6       
 #define COIN_ANIM_FRAME_TIME 0.2
 
-int main(int argc, char **argv)
-{
-    // unreferenced arguments
-    argc, argv;
+char map[NUM_TILES_Y][NUM_TILES_X] = { {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+				       {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}};
 
-    Window window = OpenWindow(800, 600, "Rudimentary Multimedia Library demo");
+void DrawTileMap(Framebuffer *fbuff)
+{
+    for(int y = 0; y < NUM_TILES_Y; y++) {
+	for(int x = 0; x < NUM_TILES_X; x++) {
+	    switch(map[y][x]) {
+	    case '0':
+		DrawRectangle(fbuff, x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE, RGB_Color(0, 255, 0));
+		break;
+	    case '1':
+		DrawRectangle(fbuff, x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE, RGB_Color(100, 100, 100));
+		break;
+	    }
+	}
+    }
+}
+
+int main(void)
+{
+    Window window = OpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Rudimentary Multimedia Library demo");
     Framebuffer fbuff = CreateFramebuffer(window.wnd_h);
     Input input = {0};
     Timer t = {0};
@@ -35,7 +64,8 @@ int main(int argc, char **argv)
 		
 	if(!MessageLoop(&input))
 	    break;
-			
+
+	DrawTileMap(&fbuff);
 	UpdateSpriteAnimation(&coin);
 	DrawBMP24bpp(&fbuff, coin.frames[coin.current_frame], 100, 100, RGB_Color(255, 0, 255));
 
