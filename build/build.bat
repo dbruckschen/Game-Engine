e737a801c3b558c6d952636a1db61e303917f886
David Bruckschen <d.bruckschen@gmail.com>
2022-07-24 13:29:25 +0200

    ...

--- build/build.bat
+++ build/build.bat
@@ -8,7 +8,7 @@ cl /Zi /W4 /c %dllfiles% /I ../include /Fo"..\obj\\"
 link /DEBUG /DLL /OUT:engine.dll ../obj/*.obj user32.lib gdi32.lib Ws2_32.lib
 
 REM copy the .lib to the test project so it always uses the newest file
-copy engine.lib ../test/build
-copy engine.dll ../test/build
+copy engine.lib ..\test\build
+copy engine.dll ..\test\build
 
 
